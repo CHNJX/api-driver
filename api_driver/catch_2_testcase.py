c4d2018839126aad8ca9d43d255f638868681ea2
CHNJX <127973718+CHNJX@users.noreply.github.com>
2023-09-07 18:02:38 +0800

    Update catch_2_testcase.py
    抓包生成用例实现

--- api_driver/catch_2_testcase.py
+++ api_driver/catch_2_testcase.py
@@ -1,144 +1,146 @@
-# -*- coding:utf-8 -*-
-# @Time     :2023/2/7 12:38
-# @Author   :CHNJX
-# @File     :command.py
-# @Desc     :命令工具
+import json
 import os
-import subprocess
-import sys
-from os.path import dirname, exists
-import click as click
+from datetime import datetime
 
+from mitmproxy import ctx
+
+from api_driver.command import har2case
 from api_driver.har_parser import HarParser
 
-sys.path.append(dirname(sys.path[0]))
 
-from api_driver.project_generator import ProjectGenerator
-from api_driver.swagger_generate import SwaggerGenerator
+class ExportFilter:
+    def __init__(self):
+        """
+        :param host: 域名
+        :param path: 路径
+        """
+        self.entries = []
+        self.exclude_url = ['js', 'png', 'gif', 'jpg', 'css', 'jpeg', 'ico']
 
-group = click.Group()
+    def load(self, loader):
+        loader.add_option(
+            name="host",
+            typespec=str,
+            default='',
+            help="Add a host as a url filter condition",
+        )
+        loader.add_option(
+            name="path",
+            typespec=str,
+            default='',
+            help="Add a path as a url filter condition",
+        )
+        loader.add_option(
+            name="name",
+            typespec=str,
+            default='',
+            help="Testcase name",
+        )
+        loader.add_option(
+            name="testcase_path",
+            typespec=str,
+            default='',
+            help="Storage path of testcase",
+        )
+        loader.add_option(
+            name="exclude",
+            typespec=str,
+            default='',
+            help="Path to be eliminated",
+        )
+        loader.add_option(
+            name="api",
+            typespec=str,
+            default='',
+            help="The path of the api-object",
+        )
 
+    def response(self, flow):
+        request_data = flow.request
+        response_data = flow.response
 
-@click.command('start_project')
-@click.option('-n', '--project-name', required=True, help='project name')
-def start_project(project_name):
-    """
-    创建项目
-    :param project_name: 项目名称
-    """
-    ProjectGenerator().project_generate(project_name)
+        # 在这里使用传递的筛选关键词进行匹配
+        host = ctx.options.host if ctx.options.host else ''
+        path = ctx.options.path.split(',') if ctx.options.path else []
 
+        if host not in request_data.pretty_url or (path and not any(p in request_data.pretty_url for p in path)):
+            return
 
-@click.command('swagger2api')
-@click.option('-s', '--swagger-doc',
-              required=True, help='Swagger doc file.')
-@click.option('-d', '--api-dir',
-              required=False, help='api save dir.')
-def swagger2api(swagger_doc, api_dir):
-    """
-    swagger文档转换成api-object
-    :param swagger_doc: swagger.json 文件
-    :param api_dir:  api存放路径 非必填
-    """
-    SwaggerGenerator().generate(swagger_doc, api_dir)
+        if any(e in request_data.pretty_url for e in self.exclude_url):
+            return
 
+        started_datetime = datetime.fromtimestamp(request_data.timestamp_start)
+        started_datetime_str = started_datetime.isoformat()
 
-@click.command('run')
-@click.option('-c', '--testcase',
-              required=True, help='testcase')
-@click.option('-t', '--tag',
-              required=False, help='testcase')
-@click.option('-r', '--reset', help='auto clear report', required=False, default='false',
-              type=click.Choice(['true', 'false']))
-@click.option('-t', '--threads', help='run with multiple threads', required=False)
-def run(testcase, tag, reset, threads):
-    """
-    运行用例并集成allure报告
-    :param threads: 运行的线程数
-    :param testcase: 用例路径
-    :param tag: 用例标签
-    :param reset: 是否重置allure报告
-    """""
-    command = f'pytest -v -s {testcase}'
-    if reset == 'true':
-        if 'win' in sys.platform:
-            subprocess.call(f'rmdir /Q /S allure-results', shell=True)
+        entry = {
+            "startedDateTime": started_datetime_str,
+            "request": {
+                "method": request_data.method,
+                "url": request_data.url,
+                "httpVersion": "HTTP/1.1",
+                "cookies": [],
+                "headers": [
+                    {"name": h[0], "value": h[1]} for h in request_data.headers.items()
+                ],
+                "queryString": [],
+                "postData": {},
+                "headersSize": -1,
+                "bodySize": len(request_data.content)
+            },
+            "response": {
+                "status": response_data.status_code,
+                "statusText": response_data.reason,
+                "httpVersion": "HTTP/1.1",
+                "cookies": [],
+                "headers": [
+                    {"name": h[0], "value": h[1]} for h in response_data.headers.items()
+                ],
+                "content": {
+                    "size": len(response_data.content),
+                    "mimeType": response_data.headers.get("Content-Type", ""),
+                    "text": response_data.text  # 存储响应文本数据
+                },
+                "redirectURL": "",
+                "headersSize": -1,
+                "bodySize": len(response_data.content)
+            },
+            "cache": {},
+            "timings": {},
+            "serverIPAddress": "",
+            "connection": "",
+            "comment": ""
+        }
+        self.entries.append(entry)
+
+    def done(self):
+        har_data = {
+            "log": {
+                "version": "1.2",
+                "creator": {
+                    "name": "mitmproxy",
+                    "version": "1.0"
+                },
+                "entries": self.entries
+            }
+        }
+        testcase_name = ctx.options.name
+        try:
+            with open(f'har/{testcase_name}.har', 'w') as har_file:
+                har_file.write(json.dumps(har_data, indent=2))
+                har_file.flush()  # 确保数据被写入磁盘
+        except Exception as e:
+            # 写入失败，处理错误
+            print(f"写入 HAR 文件时发生错误：{str(e)}")
+            # 这里可以添加处理错误的逻辑
         else:
-            subprocess.call(f'rmdir -rf allure-results', shell=True)
-    if threads:
-        command += f' -n {threads}'
-    if tag:
-        command += f' -m {tag}'
-
-    subprocess.call(command + ' --alluredir=./allure-results', shell=True)
-
-
-@click.command('har2case')
-@click.option('-h', '--har',
-              required=True, help='har file path')
-@click.option('-a', '--api',
-              required=False, help='api object dir', default=None)
-@click.option('-t', '--testcase', help='testcase dir', required=False, default='testcase')
-@click.option('-e', '--exclude', help='exclude url', required=False, default='')
-def har2case(har, api, testcase, exclude):
-    """
-    将har转换成用例
-    :param har: har文件路径
-    :param api: har对应api-object文件
-    :param testcase:  用例路径
-    :param exclude:  过来的url
-    :return:
-    """
-    hp = HarParser(har_file_path=har, api_object=api, exclude_url=exclude)
-    hp.generate_testcase(testcase_path=testcase)
+            # 写入成功，可以继续执行后续的代码
+            # 调用 shell生成测试用例 命令
+            testcase_path = ctx.options.testcase_path if ctx.options.testcase_path else 'testcase'
+            api = ctx.options.api if ctx.options.api else ''
+            exclude = ctx.options.exclude if ctx.options.exclude else ''
+            har_file_path = os.path.join(os.getcwd(), f'har/{testcase_name}.har')
+            har2case(['-h', har_file_path, '-t', testcase_path, '-a', api, '-e', exclude])
+            # 这里可以添加后续代码逻辑
 
 
-@click.command('capture2testcase')
-@click.option('-n', '--name', help="testcase name", required=True)
-@click.option('-fh', '--host', help="filter host", required=True)
-@click.option('-fp', '--path', help="filter path", required=False, default='')
-@click.option('-l', '--port', help="listening port", required=False, default='8888')
-@click.option('-h', '--har',
-              required=False, help='har file path')
-@click.option('-a', '--api',
-              required=False, help='api object dir', default='')
-@click.option('-t', '--testcase', help='testcase dir', required=False, default='testcase')
-@click.option('-e', '--exclude', help='exclude url', required=False, default='')
-def capture2testcase(name, host, path, port, har, api, testcase, exclude):
-    script_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'capture2testcase.py'))
-    # 构建 mitmproxy 命令
-    mitmproxy_command = [
-        "mitmproxy", "-s", script_path,
-        "--set", f'name={name}',
-        "--set", f'host={host}',
-        "--set", f'path={path}',
-        "--set", f'har={har}',
-        "--set", f'api={api}',
-        "--set", f'testcase_path={testcase}',
-        "--set", f'exclude={exclude}',
-        "-p", port
-    ]
-    try:
-        # 启动 mitmproxy 命令
-        subprocess.run(mitmproxy_command, check=True, shell=True)
-    except subprocess.CalledProcessError as e:
-        # 处理命令执行失败的情况
-        click.echo(f"命令执行失败，返回代码 {e.returncode}")
-    except Exception as ex:
-        # 处理其他异常
-        click.echo(f"发生异常：{str(ex)}")
-
-
-group.add_command(start_project)
-group.add_command(swagger2api)
-group.add_command(run)
-group.add_command(har2case)
-group.add_command(capture2testcase)
-
-
-def cmd():
-    group.main()
-
-
-if __name__ == '__main__':
-    har2case()
+addons = [ExportFilter()]
